de94fdc1150874d49e060fa0bc2350e3413d643f
deggion <deggiondeggion@gmail.com>
2022-07-12 12:53:14 +0300

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,80 @@
+// // Import vendor jQuery plugin example
+// import '~/app/libs/mmenu/dist/mmenu.js'
+
+import 'slick-carousel'
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	$('.header-btn').on('click', function () {
+		$('.header-btn').toggleClass('opened')
+		$('.mobile-nav').toggleClass('show')
+	})
+
+	$('.main-slider').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: true,
+		autoplay: false,
+		autoplaySpeed: 9000,
+	});
+
+
+	const windowWidth = $('body').innerWidth()
+
+	if(windowWidth > 991){
+		$('.products-slider.only-desktop').slick({
+			infinite: true,
+			slidesToShow: 4,
+			slidesToScroll: 1,
+			dots: false,
+			autoplay: false,
+			autoplaySpeed: 7000,
+		});
+	}
+
+	$('.products-slider.desktop-mobile').slick({
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		dots: false,
+		autoplay: false,
+		autoplaySpeed: 7000,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 3,
+				}
+			},
+			{
+				breakpoint: 576,
+				settings: {
+					slidesToShow: 2,
+				}
+			}
+		]
+	});
+
+	$('.brands-slider').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		dots: false,
+		autoplay: false,
+		autoplaySpeed: 5000,
+		responsive: [
+			{
+				breakpoint: 576,
+				settings: {
+					arrows: false
+				}
+			}
+		]
+	});
+
+	$('.navigation-filter__head').on('click', function () {
+		$(this).parent().toggleClass('active')
+	})
+
+})
